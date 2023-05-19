2ec2162c282226b8aa345b789d879301bda2b809
alval90 <alval90@users.noreply.github.com>
2023-05-19 15:39:41 +0200

    use correct image

--- lib/views/sessionCountdownView.dart
+++ lib/views/sessionCountdownView.dart
@@ -53,7 +53,7 @@ class KeyVisual extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset("assets/images/learning.jpg", height: 233, width: 290);
+    return Image.asset("assets/images/learning.png", height: 233, width: 290);
   }
 }
 
