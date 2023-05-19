269045be7903c6e6a6cde1aea018859ac6753207
alval90 <alval90@users.noreply.github.com>
2023-05-19 15:32:32 +0200

    remove red colors

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ final sharedPrefs = SharedPrefs();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode (SystemUiMode.manual, overlays: [SystemUiOverlay.top]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);
   await sharedPrefs.init();
   runApp(const BUDdyApp());
 }
@@ -19,7 +20,7 @@ class BUDdyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BUDdy App',
-      themeMode:ThemeMode.system,
+      themeMode: ThemeMode.system,
       theme: Themes.lightTheme,
       darkTheme: Themes.darkTheme,
       home: const SplashScreenView(),
@@ -27,35 +28,16 @@ class BUDdyApp extends StatelessWidget {
   }
 }
 
-
 class Themes {
   static bool useMaterial3 = true;
   static String fontFamily = 'Lato';
   static TextTheme textTheme = const TextTheme(
-    displayLarge: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.w400,
-        color: Color(0xffff0000)), // to see missing use of context colorScheme
-    bodyLarge: TextStyle(
-        fontSize: 57.0,
-        fontWeight: FontWeight.w400,
-        color: Color(0xffff0000)),
-    bodyMedium: TextStyle(
-        fontSize: 16.0,
-        fontWeight: FontWeight.w400,
-        color: Color(0xffff0000)),
-    bodySmall: TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: Color(0xffff0000)),
-    labelLarge: TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w500,
-        color: Color(0xffff0000)),
-    labelMedium: TextStyle(
-        fontSize: 12.0,
-        fontWeight: FontWeight.w500,
-        color: Color(0xffff0000)),
+    displayLarge: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w400),
+    bodyLarge: TextStyle(fontSize: 57.0, fontWeight: FontWeight.w400),
+    bodyMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+    bodySmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400),
+    labelLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+    labelMedium: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w500),
   );
   static ThemeData lightTheme = ThemeData(
     useMaterial3: useMaterial3,
@@ -85,11 +67,12 @@ class Themes {
         onSurface: const Color(0xff1F1B16),
         surfaceVariant: const Color(0xffF1E0D0),
         onSurfaceVariant: const Color(0xff504539),
-        inverseSurface: const Color(0xffF6ECE4), // surfaceContainer (not supported anymore) in design
+        inverseSurface: const Color(0xffF6ECE4),
+        // surfaceContainer (not supported anymore) in design
         surfaceTint: const Color(0xffFCF2EA),
         outline: const Color(0xff827568),
         outlineVariant: const Color(0xffD4C4B5)),
-    );
+  );
 
   static ThemeData darkTheme = ThemeData(
     useMaterial3: useMaterial3,
@@ -100,7 +83,8 @@ class Themes {
         onPrimary: const Color(0xff472A00),
         primaryContainer: const Color(0xff653E00),
         onPrimaryContainer: const Color(0xffFFDDB8),
-        inversePrimary: const Color(0xffFFB960), //??
+        inversePrimary: const Color(0xffFFB960),
+        //??
         secondary: const Color(0xffAAD46C),
         onSecondary: const Color(0xff213600),
         secondaryContainer: const Color(0xff324F00),
@@ -119,8 +103,10 @@ class Themes {
         onSurface: const Color(0xffEBE1D9),
         surfaceVariant: const Color(0xff504539),
         onSurfaceVariant: const Color(0xffD4C4B5),
-        inverseSurface: const Color(0xffF6ECE4), // surfaceContainer (not supported anymore) in design ??
-        surfaceTint: const Color(0xffFCF2EA), // ??
+        inverseSurface: const Color(0xffF6ECE4),
+        // surfaceContainer (not supported anymore) in design ??
+        surfaceTint: const Color(0xffFCF2EA),
+        // ??
         outline: const Color(0xff504539),
         outlineVariant: const Color(0xffD4C4B5)), // ??
   );
