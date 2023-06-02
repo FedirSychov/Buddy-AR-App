35b358fce5e2873718e98f1ce6586cb4f61fe01e
alval90 <23277094+alval90@users.noreply.github.com>
2023-06-02 13:34:36 +0200

    refactor: setting durations (#48)
    * refactor: setting session and activity durations
    
    * refactor: move time intialization to viewModel of ongoing views
    
    ---------
    
    Co-authored-by: alval90 <alval90@users.noreply.github.com>

--- lib/viewModels/ongoingActivityViewModel.dart
+++ lib/viewModels/ongoingActivityViewModel.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../clients/notificationClient.dart';
+import '../clients/sharedPrefs.dart';
 
 class OngoingActivityViewModel {
+  int breakDuration = SharedPrefs().getBreakDuration();
+
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
@@ -13,5 +16,15 @@ class OngoingActivityViewModel {
   void showBigTextNotification(String title, String body) {
     NotificationClient.showBigTextNotification(
         title: title, body: body, fln: flutterLocalNotificationsPlugin);
+  }
+
+  DateTime getInitDateTimeUntil() {
+    return DateTime.now().add(Duration(seconds: breakDuration));
+  }
+
+  Duration getInitRemainingDuration() {
+    return DateTime.now()
+        .add(Duration(seconds: breakDuration))
+        .difference(DateTime.now());
   }
 }
